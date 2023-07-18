49a0a18ac7b1f044133d79c96c3898eba15d8a18
spunk-developer <xspunk.developer@gmail.com>
2023-07-18 13:29:28 +0300

    feat: Reimplement budget prompt fully

--- airdrop/preflight.py
+++ airdrop/preflight.py
@@ -1,6 +1,8 @@
 """Operations, otherwise known as steps that the airdrop program has to take to complete it's task."""
 """Author: spunk-developer <xspunk.developer@gmail.com>"""
 
+from rich.prompt import Prompt
+
 from airdrop.calc import update_budget
 
 def preflight_validate_supply_balance(input) -> None:
@@ -13,18 +15,21 @@ def preflight_validate_supply_balance(input) -> None:
         RuntimeError: If `input` value cannot be converted into a number, is not within range of `0` to `100000000000000000` or if the budget variable has already been defined.
     """
 
+    # In the case that balance wasn't passed into the CLI as a parameter, we ask the user directly.
+    input = Prompt.ask("Enter the total airdrop budget", default=0)
+
     # We parse the actual input into a float or an int. If this fails, the error is raised to the caller.
     try:
         if type(input) is not (float | int):
-            input = float(input)
+            validated_input = float(input)
     except:
         raise RuntimeError(f'Could not converty input "{ input }" into a number')
 
     # XRP minimum and maximum number enforcement.
-    if input <= 0:
-        raise RuntimeError(f'Input "{ input }" is equal to or smaller than 0')
-    elif input > 100000000000000000:
-        raise RuntimeError(f'Input "{ input }" is larger than maximum allowed integer 100000000000000000')
+    if validated_input <= 0:
+        raise RuntimeError(f'Input "{ input }" must be larger than 0')
+    elif validated_input > 100000000000000000:
+        raise RuntimeError(f'Input "{ input }" cannot be larger than maximum allowed integer 100000000000000000')
 
-    if not update_budget(input):
+    if not update_budget(validated_input):
         raise RuntimeError("Cannot overwrite supply budget, as it has already been defined")
